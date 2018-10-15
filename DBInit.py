af7425fcf1917b457213d6be5ba2df5bd767949d
Xam <thorn.ryzome@gmail.com>
2018-10-15 04:09:59 -0600

    Tool Files Added

---
+++ DBInit.py
@@ -0,0 +1,39 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+    return None
+
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def main():
+    print('\n BOT DB INIT')
+    sql_create_users_table = """ CREATE TABLE IF NOT EXISTS users (
+                                        id integer PRIMARY KEY,
+                                        name text NOT NULL,
+                                        bees integer NOT NULL
+                                        ); """
+ 
+
+    conn = create_connection("C:\\Users\\Thorn\\Desktop\\BOTS\\Beeees\\DB\\Money.db")
+    if conn is not None:
+        create_table(conn, sql_create_users_table)
+    else:
+        print("Error! cannot create the database connection.")
+
+
+if __name__ == '__main__':
+    main()
